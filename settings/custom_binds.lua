ea2cb840c55311fa9ea8da200c3993a49542c5c0
Daniel Sont <dan.sont@gmail.com>
2016-09-03 12:03:22 -0400

    adding some custom binds for my dasKeyboard scroll wheel

--- settings/custom_binds.lua
+++ settings/custom_binds.lua
@@ -1,5 +1,14 @@
 local awful = require("awful")
 
+-- xev | grep -A2 --line-buffered '^KeyRelease' | sed -n '/keycode /s/^.*keycode \([0-9]*\).* (.*, \(.*\)).*$/\1 \2/p'
+local XF86AudioLowerVolume = "#122"
+local XF86AudioRaiseVolume = "#123"
+local XF86AudioPrev = "#173"
+local XF86AudioPlay = "#172"
+local XF86AudioNext = "#171"
+local XF86Sleep = "#150"
+local XF86AudioMute = "#121"
+
 local numpad = { "#87", "#88", "#89", "#83", "#84", "#85", "#79", "#80", "#81" }
 local alt = "Mod1"
 local mod = "Mod4"
@@ -13,12 +22,16 @@ globalkeys = awful.util.table.join(globalkeys,
 
     -- awful.key({ mod, }, "p", function () awful.util.spawn('bash -ic \"xfce4-appfinder\"') end),
 
+    awful.key({  }, XF86AudioLowerVolume, awful.tag.viewprev),
+    awful.key({  }, XF86AudioRaiseVolume, awful.tag.viewnext),
+    awful.key({ mod }, XF86AudioLowerVolume, function () awful.screen.focus_relative( 1) end),
+    awful.key({ mod }, XF86AudioRaiseVolume, function () awful.screen.focus_relative(-1) end),
     awful.key({ mod, }, "Delete", function () awful.util.spawn("bash -ic \"zenrun\"") end),
     awful.key({ mod, }, "Return", function () awful.util.spawn("terminator") end),
 
     awful.key({ mod, }, "w", function () mymainmenu:show() end),
 
-    awful.key({ mod, }, "s", function () awful.util.spawn("scrot", "-s") end),    
+    awful.key({ mod, }, "s", function () awful.util.spawn("scrot", "-s") end),
 
     awful.key({ mod, }, "Tab",
         function ()
