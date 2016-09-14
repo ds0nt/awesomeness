539f8df5011a1b2c5fea0f2b9770f5a4a925cd8c
Daniel Sont <dan.sont@gmail.com>
2016-09-14 16:07:50 -0400

    infinite screens

--- settings/tags.lua
+++ settings/tags.lua
@@ -13,18 +13,13 @@ layouts =
   lain.layout.cascade,
 }
 
-first_tags = {
-  names  = { " 1 ", " 10 ", " 11 ", " 100 ", " 101 ", " 110 ", " 111 " },
-  layouts = layouts
-}
-
 default_tags = {
   names  = { " 1 ", " 10 ", " 11 ", " 100 ", " 101 ", " 110 ", " 111 " },
   layouts = layouts
 }
 
 tags = {}
-tags[1] = awful.tag(first_tags.names, 1, first_tags.layouts)
-if screen.count() > 1 then
-  tags[2] = awful.tag(default_tags.names, 2, default_tags.layouts)
+for i=1,screen.count() do
+  -- body...
+  tags[i] = awful.tag(default_tags.names, i, default_tags.layouts)
 end
